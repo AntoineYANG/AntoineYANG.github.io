31c94bc5ca6c787832c0de8658ba2ff7850914bd
AntoineYANG <814602569@qq.com>
2020-04-13 02:30:54 +0800

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello！ This is Antoine YANG's blog.
+		</p>
+		<p>
+		  Sorry that this page is still under developing.
+		</p>
+		<p>
+		  Hope to see you here in the near future. (´∀｀)
+		</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
