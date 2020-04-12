4ddc721df22c3fcd44219ba64571e37776efd789
AntoineYANG <814602569@qq.com>
2020-04-13 02:53:51 +0800

    Added the first built project to branch

---
+++ build/precache-manifest.dfdddfad6c7d647316e3aba61d61f7f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73eab59fbe4f02870ec560a940002138",
+    "url": "./index.html"
+  },
+  {
+    "revision": "01a7a971ca3c1054f3ee",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5920c032d4157ae032b7",
+    "url": "./static/js/2.a8628e5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a8628e5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01a7a971ca3c1054f3ee",
+    "url": "./static/js/main.251ab8a1.chunk.js"
+  },
+  {
+    "revision": "8bc83efdb58e24790ea0",
+    "url": "./static/js/runtime-main.b3a575e4.js"
+  }
+]);
