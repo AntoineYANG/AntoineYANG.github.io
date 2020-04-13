b9bdfc3a6e50fcac8afd8ea4a63b002a6ae2fd7b
AntoineYANG <814602569@qq.com>
2020-04-13 19:39:23 +0800

    Check if router is available

---
+++ precache-manifest.d1e00afc3fe254ee843be54903ba98e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2810d05061a39f8d66795e6bed40a0d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d40280d3548ddb2f59a2",
+    "url": "./static/css/main.ec972626.chunk.css"
+  },
+  {
+    "revision": "32130e896a63770d4e2a",
+    "url": "./static/js/2.69af229c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.69af229c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d40280d3548ddb2f59a2",
+    "url": "./static/js/main.a1bbe1ca.chunk.js"
+  },
+  {
+    "revision": "8bc83efdb58e24790ea0",
+    "url": "./static/js/runtime-main.b3a575e4.js"
+  }
+]);
