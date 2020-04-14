85500b66399bb7398a305198d00fd88a02b15c1f
AntoineYANG <814602569@qq.com>
2020-04-14 18:47:49 +0800

    Here comes a rain

---
+++ precache-manifest.50c2b0536783290632d96efe486959cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40972c631cde57808ba8af1c2d53a41b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "96c51202ba7b893f4868",
+    "url": "./static/css/main.c54be9d4.chunk.css"
+  },
+  {
+    "revision": "a5c138d034a4530e31c1",
+    "url": "./static/js/2.920d971c.chunk.js"
+  },
+  {
+    "revision": "890b83c3b503af9a5630afd9ff112890",
+    "url": "./static/js/2.920d971c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96c51202ba7b893f4868",
+    "url": "./static/js/main.2bebeafb.chunk.js"
+  },
+  {
+    "revision": "8bc83efdb58e24790ea0",
+    "url": "./static/js/runtime-main.b3a575e4.js"
+  }
+]);
