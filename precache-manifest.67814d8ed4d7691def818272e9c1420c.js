44abb1fed6dfaa3a6073ef6feb20512d54a50818
AntoineYANG <814602569@qq.com>
2020-04-15 22:41:00 +0800

    Build up back-end

---
+++ precache-manifest.67814d8ed4d7691def818272e9c1420c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcebc5f82b289bd28b2ac146c6a65bb0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b0beaa92e2f585261b29",
+    "url": "./static/css/main.ecd193e6.chunk.css"
+  },
+  {
+    "revision": "0b336bbf5e87d80a83c5",
+    "url": "./static/js/2.1a7bb8a4.chunk.js"
+  },
+  {
+    "revision": "890b83c3b503af9a5630afd9ff112890",
+    "url": "./static/js/2.1a7bb8a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0beaa92e2f585261b29",
+    "url": "./static/js/main.4ed7628d.chunk.js"
+  },
+  {
+    "revision": "8bc83efdb58e24790ea0",
+    "url": "./static/js/runtime-main.b3a575e4.js"
+  }
+]);
