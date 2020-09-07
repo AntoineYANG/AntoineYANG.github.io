8964b795b9a700ee0f33d8935461ab1d75f95c67
AntoineYANG <814602569@qq.com>
2020-09-07 15:59:45 +0800

    Completing features of video player

--- precache-manifest.2014ef8640fcb9682d1bcb1d7f711aeb.js
+++ precache-manifest.2014ef8640fcb9682d1bcb1d7f711aeb.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c4435d69280a075c08a75b1e771182cb",
+    "revision": "69c6c9e1298e954a508b39de50bb42e6",
     "url": "/index.html"
   },
   {
-    "revision": "9f1076e7885c782fd15d",
+    "revision": "d5c1fb246bf987b85e87",
     "url": "/static/css/main.0c768c5c.chunk.css"
   },
   {
-    "revision": "5bda28bac60b0baaee74",
-    "url": "/static/js/2.28dd3a40.chunk.js"
+    "revision": "77f060d1c135a90f7c63",
+    "url": "/static/js/2.8eaac674.chunk.js"
   },
   {
     "revision": "bd7fe678165fe533a884479313d73b08",
-    "url": "/static/js/2.28dd3a40.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.8eaac674.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9f1076e7885c782fd15d",
-    "url": "/static/js/main.2cad7f7d.chunk.js"
+    "revision": "d5c1fb246bf987b85e87",
+    "url": "/static/js/main.504e88ce.chunk.js"
   },
   {
     "revision": "ec9875811aa0f00c2e31",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9fd7c87d758f9547cccf1b7b7c8bbad0.js"
+  "/precache-manifest.2014ef8640fcb9682d1bcb1d7f711aeb.js"
 );
 
 self.addEventListener('message', (event) => {
